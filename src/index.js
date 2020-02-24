26d04ea3d27c7ad3dd3cb940d47f4723798a1f81
lzk2636 <2820421644@qq.com>
2020-02-24 11:41:14 +0800

    render-props模式运用

--- src/index.js
+++ src/index.js
@@ -20,13 +20,20 @@ class Mouse extends Component{
         })
     }
     render(){
-        return (
-            <p>鼠标的位置:x:{this.state.x}===y:{this.state.y}</p>
-        )
+        return this.props.show(this.state)
+        
     }
 }
-function Cat (){
-    return <img src={require('./components/index.png')} alt="pic" style={{width:"50px",height:"50px"}}/>
+function Cat (props){
+    return <img src={require('./components/index.png')} alt="pic" 
+    style={{width:"50px",height:"50px",position:"fixed",left:props.x-25,top:props.y-25}}/>
 }
+/**在
+* @description:render-props模式
+    本质是在mouse 的组件中封装了一个方法,返回一个自定义方法交给props
+    在自定义方法返回一个调用一个组件,并获取返回的的参数放在cat中
+* @param {type} 
+* @return: 
+*/
 
-RectDom.render(<Cat />,document.getElementById('root'))
+RectDom.render(<Mouse  show={(state)=><Cat {...state} />} />,document.getElementById('root'))
